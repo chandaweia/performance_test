cd41d7cdddd44909bcb751ca8645d2dac7a87945
weicuidi <weicuidi@node1.onvm3nodes.gwcloudlab-pg0.wisc.cloudlab.us>
2020-06-25 07:50:05 -0500

    v2

--- cal.sh
+++ cal.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
+#./cal.sh result/wrk1.txt 1k
 
-PerfPATH=/users/weicuidi/performance_test/wrk1.txt
-TMPPATH=/users/weicuidi/tmp.txt
-RESULT=/users/weicuidi/result1.txt
+PerfPATH=/users/weicuidi/performance_test/result/wrk1.txt
+TMPPATH=/users/weicuidi/performance_test/tmp.txt
+RESULT=/users/weicuidi/performance_test/result1.txt
 
-echo "Threads  Concurrency  File  50th_latency  99th_latency  Throughput" > $RESULT 
+echo "Threads,Concurrency,File,50th_latency,99th_latency,Throughput" > $RESULT 
 
 function get_tc()
 {
-	echo "get_tc"
-	echo $@
+	echo -n "$1,$4," >> $RESULT
+}
+function write_file()
+{
+	echo -n "$1," >> $RESULT
+}
+function write_latency()
+{
+	echo -n "$2," >> $RESULT
+}
+function write_throughput()
+{
+	echo -n $2 >> $RESULT
 }
 
 function readfile()
 {
+	echo "filename:$1 filesize:$2"
 	while read line
-		case $line in
-			Running*): echo >> $RESULT;;
-			*threads*connections*): t_c;;
-
-		esac
-
 	do
+		if [[ $line == Running* ]]
+		then
+			echo "">>$RESULT
+		elif [[ $line == *threads* ]] && [[ $line == *connections* ]]
+		then
+			get_tc $line
+			write_file $2
+		elif [[ $line == 50%* ]]
+		then
+			write_latency $line
+		elif [[ $line == 99%* ]]
+		then
+			write_latency $line
+		elif [[ $line == Requests/sec* ]]
+		then
+			write_throughput $line
+		fi
+
 	done < $PerfPATH
 }
 
-readfile
+#readfile $1 $2
+readfile $PerfPATH 1k
--- perform.sh
+++ perform.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-wrk -t4 -c20 -d60s -T30s --latency http://192.168.1.4 > wrk1.txt
-sleep(10)
-wrk -t4 -c100 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t4 -c500 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t4 -c1000 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
+FILEPATH=./result/wrk1.txt
+wrk -t4 -c20 -d60s -T30s --latency http://192.168.1.4 > $FILEPATH
+sleep 10
+wrk -t4 -c100 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t4 -c500 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t4 -c1000 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
 
-wrk -t10 -c20 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t10 -c100 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t10 -c500 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t10 -c1000 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
+wrk -t10 -c20 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t10 -c100 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t10 -c500 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t10 -c1000 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
 
-wrk -t20 -c20 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t20 -c100 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t20 -c500 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
-wrk -t20 -c1000 -d60s -T30s --latency http://192.168.1.4 >> wrk1.txt
-sleep(10)
+wrk -t20 -c20 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t20 -c100 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t20 -c500 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
+wrk -t20 -c1000 -d60s -T30s --latency http://192.168.1.4 >> $FILEPATH
+sleep 10
