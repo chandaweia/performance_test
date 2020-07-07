21fa1761bba3338d35e8c8df1843b5a4d0f9d141
weicuidi <weicuidi@node1.onvm3nodes.gwcloudlab-pg0.wisc.cloudlab.us>
2020-07-07 11:24:20 -0500

    pubsub

--- calwrk2.sh
+++ calwrk2.sh
@@ -3,9 +3,9 @@
 #./cal.sh result/wrk1.txt 1  
 
 WrkPATH=/users/weicuidi/performance_test/result
-RESULT=/users/weicuidi/performance_test/pubsub-test.csv
+RESULT=/users/weicuidi/performance_test/$2
 
-echo "Threads,Concurrency,Request,File_k,50th_latency,99th_latency,Throughput" > $RESULT 
+echo "Threads,Concurrency,Request,File_k,50th_latency,99th_latency,Throughput" > $RESULT
 
 function write_latency()
 {
@@ -28,27 +28,30 @@ function readfile()
 		if [[ $line == wrk* ]]
 		then
 			getwrk $line
-			echo $line
+			#echo $line
 		elif [[ $line == 50.000%* ]]
 		then
-			echo $line
+			#echo $line
 			write_latency $line
 		elif [[ $line == 99.000%* ]]
 		then
-			echo $line
+			#echo $line
 			write_latency $line
 		elif [[ $line == Requests/sec* ]]
 		then
-			echo $line
+			#echo $line
 			write_throughput $line
 		fi
 
 	done < $1
 }
 
+echo $RESULT
+readfile $WrkPATH/$1
+
 #readfile $1 $2
 #readfile $1 $WrkPATH/$1 
-echo $RESULT
-readfile $WrkPATH/pubsub-test.txt 
+#echo $RESULT
+#readfile $WrkPATH/pubsub-test.txt 
 #readfile $WrkPATH/bridge-11k-1593455789.txt 11 
 #readfile $WrkPATH/bridge-111k-1593455789.txt 111 
--- wrk2_test.sh
+++ wrk2_test.sh
@@ -3,31 +3,21 @@ timestamp=$(date +%s)
 echo $timestamp
 FILEPATH=./result/$1-11k-$timestamp.txt
 >$FILEPATH
+echo $FILEPATH
 
 URL1=http://192.168.1.4/index.html
 URL2=http://192.168.1.4/index1.html
 URL3=http://192.168.1.4/index111.html
-for i in 500 1000 3000 5000 10000 150000 20000
+#Requests
+for i in 500 5000 10000 20000 30000
 do      
         echo "Welcome $i times"
-
-	for j1 in 4 10 20
-        do
-                for k1 in  20 100 500 1000
-                do
-                        echo "wrk -t ${j1} -c ${k1} -d30s -R ${i} -file_k 1 --latency $URL2" >> $FILEPATH
-                        wrk -t${j1} -c${k1} -d30s -R${i} --latency $URL2 >> $FILEPATH
-                done
-                sleep 10
-        done
-
-	for j2 in 4 10 20
-        do
-                for k2 in  20 100 500 1000
-                do
-                        echo "wrk -t ${j2} -c ${k2} -d30s -R ${i} -file_k 111 --latency $URL3" >> $FILEPATH
-                        wrk -t${j2} -c${k2} -d30s -R${i} --latency $URL3 >> $FILEPATH
-                done
-                sleep 10
-        done
+	echo "wrk -t 20 -c 100 -d30s -R ${i} -file_k 11 --latency $URL1"
+	echo "wrk -t 20 -c 100 -d30s -R ${i} -file_k 11 --latency $URL1" >> $FILEPATH
+	wrk -t20 -c100 -d30s -R${i} --latency $URL1 >> $FILEPATH
+	sleep 10
+	#wrk -t20 -c100 -d30s -R${i} --latency $URL2 >> $FILEPATH
+        #sleep 10
+	#wrk -t20 -c100 -d30s -R${i} --latency $URL3 >> $FILEPATH
+        #sleep 10
 done
