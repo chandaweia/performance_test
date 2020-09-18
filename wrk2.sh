2bbf954567459d31e95e59993d958a5854072486
root <root@node1.pubsubmos.gwcloudlab-pg0.wisc.cloudlab.us>
2020-09-17 19:43:19 -0500

    pubsub

--- wrk2.sh
+++ wrk2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #timestamp=$(date +%s)
-filesize=11
+filesize=0.6
 timestamp=$(date '+%Y%m%d%H%M%S')
 echo $timestamp
-FILEPATH=./n2nresult/$1-612-$timestamp.txt
-REPORT=./report/$1-612-$timestamp.csv
+FILEPATH=./n2nresult/$1-${filesize}k-$timestamp.txt
+REPORT=./report/$1-${filesize}k-$timestamp.csv
 
 #FILEPATH=./n2nresult/$1-11k-$timestamp.txt
 #REPORT=./report/$1-11k-$timestamp.csv
@@ -18,13 +18,13 @@ function wrk_test()
 	for i in 30000 70000 100000 150000 200000 300000 400000 500000 600000
 	do      
         	echo "Welcome $i times"
-		echo "wrk -t 40 -c 100 -d30s -R ${i} -file_k 612 --latency $URL1"
+		echo "wrk -t 40 -c 100 -d30s -R ${i} -file_k ${filesize} --latency $URL1"
 
 		#echo "wrk -t 20 -c 100 -d30s -R ${i} -file_k 11 --latency $URL1" >> $FILEPATH
 		#wrk -t20 -c100 -d30s -R${i} --latency $URL1 >> $FILEPATH
 		#sleep 10
 
-		echo "wrk -t 40 -c 100 -d30s -R ${i} -file_k 612 --latency $URL1" >> $FILEPATH
+		echo "wrk -t 40 -c 100 -d30s -R ${i} -file_k ${filesize} --latency $URL1" >> $FILEPATH
 		wrk -t40 -c100 -d30s -R${i} --latency $URL1 >> $FILEPATH
         	sleep 10
 
@@ -32,6 +32,7 @@ function wrk_test()
         	#sleep 10
 		#wrk -t20 -c100 -d30s -R${i} --latency $URL3 >> $FILEPATH
         	#sleep 10
+		
 	done
 }
 
